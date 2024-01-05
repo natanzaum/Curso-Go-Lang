774e7ce9f1a8b78bd0844cec34611ac794c5f457
natan <natan@DESKTOP-0MHIMNT>
2024-01-05 19:07:23 -0300

    Adição do primeiro aplicativo

---
+++ Aplicação De Linha De Comando/app/app.go
@@ -0,0 +1,70 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de linha de comando"
+	app.Usage = "Busca de Nomes e IPs de servidores na internet"
+
+	app.Commands = []cli.Command{
+		{Name: "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+		{
+			Name:  "servidores",
+			Usage: "Busca servidores na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: buscaServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscaServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
+
+}
